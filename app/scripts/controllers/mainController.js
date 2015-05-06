29b51b288584278733e279dfc1345d0ceaa4b3b5
charlesarmitage <code@cjrarmitage.co.uk>
2015-05-06 22:33:01 +0100

    Fix jsHint warnings

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -1,14 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name PottyPottyPotty.controller:MainController
- * @description
- * # MainController
- */
 angular.module('PottyPottyPotty')
-  .controller('MainController', function($scope) {
-
-    // do something with $scope
-
+  .controller('MainController', function() {
   });
--- app/scripts/controllers/settingsController.js
+++ app/scripts/controllers/settingsController.js
@@ -1,14 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name PottyPottyPotty.controller:SettingsController
- * @description
- * # SettingsController
- */
 angular.module('PottyPottyPotty')
-  .controller('SettingsController', function($scope) {
-
-    // do something with $scope
-
+  .controller('SettingsController', function() {
   });
