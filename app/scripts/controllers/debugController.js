1073cc685a090c5e38bc4efae34cd01fe3fa9a05
Julian Churchill <chewielouie03@gmail.com>
2015-07-12 22:29:14 +0100

    Removed  comment, not needed

--- app/scripts/controllers/debugController.js
+++ app/scripts/controllers/debugController.js
@@ -4,11 +4,5 @@ angular.module('PottyPottyPotty')
   .controller('DebugController', function($scope, pottyTrips) {
     $scope.resetTrips = function(){
         pottyTrips.resetTrips();
-        // TODO: Should this be in a '$apply' so it notifies
-        //       angular of data changes?
-        //       When added to an apply it never seems to execute...
-        // $scope.$apply(function() {
-            // pottyTrips.resetTrips();
-        // });
     };
   });
