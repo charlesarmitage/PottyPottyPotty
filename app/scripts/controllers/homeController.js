b14f2a9b970ce77dc1e3eb6b68bdaef963f33e33
Julian Churchill <chewielouie03@gmail.com>
2015-07-12 22:10:16 +0100

    Added watch on pottyTrips for home view

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -3,4 +3,9 @@
 angular.module('PottyPottyPotty')
   .controller('HomeController', function($scope, pottyTrips) {
     $scope.pottyTrips = pottyTrips.trips();
-  });
+    $scope.$watch(function () { return pottyTrips.trips(); }, function (newVal, oldVal) {
+        if (newVal !== oldVal) {
+            $scope.pottyTrips = pottyTrips.trips();
+        }
+    });
+});
