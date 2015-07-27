847572d7642f069c3ee81b4ae3897d2464374967
Julian Churchill <chewielouie03@gmail.com>
2015-07-27 21:23:53 +0100

    Moved trip type text from home.html to controller

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -8,18 +8,24 @@ angular.module('PottyPottyPotty')
         $scope.pottyTrips = pottyTrips.trips();
         for (var i = 0; i < $scope.pottyTrips.length; i++) {
             var trip = $scope.pottyTrips[i];
+            trip.typeText = '';
+            if( trip.isWee && trip.isPoo ) {
+                trip.typeText = 'Wee & Poo';
+            } else if( trip.isWee ) {
+                trip.typeText = 'Wee';
+            } else if( trip.isPoo ) {
+                trip.typeText = 'Poo';
+            }
             trip.previousWeeText = $filter('amDifference')(trip.timestamp, trip.timestampOfPreviousWee, 'minutes');
             if( trip.previousWeeText === 1 ) {
                 trip.previousWeeText += ' minute';
-            }
-            else {
+            } else {
                 trip.previousWeeText += ' minutes';
             }
             trip.previousPooText = $filter('amDifference')(trip.timestamp, trip.timestampOfPreviousPoo, 'minutes');
             if( trip.previousPooText === 1 ) {
                 trip.previousPooText += ' minute';
-            }
-            else {
+            } else {
                 trip.previousPooText += ' minutes';
             }
         }
