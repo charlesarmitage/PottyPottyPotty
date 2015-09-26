1990b730651befb2b223e0e5e603715306b62887
charlesarmitage <code@cjrarmitage.co.uk>
2015-09-26 22:35:37 +0100

    Present timestamp as local time on home page

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('PottyPottyPotty')
-  .controller('HomeController', function($scope, $filter, $ionicPopup, pottyTrips) {
+  .controller('HomeController', function($scope, $filter, $ionicPopup, pottyTrips, TimestampService) {
     $scope.pottyTrips = [];
 
     $scope.loadPagedPottyTrips = function(){
         var trips = pottyTrips.trips();
         for (var i = 0; i < trips.length; i++) {
             var trip = trips[i];
+
             trip.typeText = '';
             if( trip.isWee && trip.isPoo ) {
                 trip.typeText = 'Wee & Poo';
@@ -16,18 +17,23 @@ angular.module('PottyPottyPotty')
             } else if( trip.isPoo ) {
                 trip.typeText = 'Poo';
             }
+
             trip.previousWeeText = $filter('amDifference')(trip.timestamp, trip.timestampOfPreviousWee, 'minutes');
             if( trip.previousWeeText === 1 ) {
                 trip.previousWeeText += ' minute';
             } else {
                 trip.previousWeeText += ' minutes';
             }
+
             trip.previousPooText = $filter('amDifference')(trip.timestamp, trip.timestampOfPreviousPoo, 'minutes');
             if( trip.previousPooText === 1 ) {
                 trip.previousPooText += ' minute';
             } else {
                 trip.previousPooText += ' minutes';
             }
+
+            trip.timestampAsLocal = TimestampService.convert.secsToDate(
+                                     TimestampService.convert.toLocal(trip.timestamp / 1000));
         }
         $scope.pottyTrips = trips;
     };
